30e0ac4119ce9924c21be23ba11b4596de1e09d1
unknown <uxob03@outlook.com>
2021-11-30 19:42:43 -0500

    partially coming back to reality. created user/profile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+
+import User from './components/User';
 class App extends React.Component {
   render() {
     return(<div>
-      Github Card
+      User Information
+      <User />
     </div>);
   }
 }
---
+++ src/components/User.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class User extends Component {
+    render() {
+        return (
+            <div>
+                <input />
+                <button>Search</button>
+            </div>
+        )
+    }
+}
